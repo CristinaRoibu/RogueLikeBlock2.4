791fa69c528f375184de532bf171b1b8bc999fa2
CristinaRoibu <kristinna@hotmail.it>
2018-07-29 19:02:28 +0200

    StartScene
    Startscne added.
    Start and quitbuttons added
    (The analytics trackers are not in here because of some issues)

---
+++ Assets/_Complete-Game/Scripts/SartScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SartScene : MonoBehaviour {
+
+	// Use this for initialization
+	public void StarButton () {
+        SceneManager.LoadScene("Main");
+	}
+	
+	// Update is called once per frame
+	public void QuitButton () {
+        Application.Quit();
+
+	}
+}
